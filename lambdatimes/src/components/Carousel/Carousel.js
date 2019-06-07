a09709fc6e36391187a93a2d651a2fdb73e40a86
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-06-07 13:03:34 -0400

    added basic functionality to carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,39 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carousel: [],
+      selected: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      carousel: carouselData,
+    })
   }
 
   leftClick = () => {
+    this.setState(prevState => ({
+      selected: prevState.selected - 1
+    }))
 
   }
 
   rightClick = () => {
+    this.setState(prevState => ({
+      selected: prevState.selected + 1
+    }))
 
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.carousel[this.state.selected]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <img src={this.state.carousel[this.state.selected]} style={{display: 'block'}} />
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
